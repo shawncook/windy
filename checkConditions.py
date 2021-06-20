772c300a523ce78774708cee5aaf3bc709e63e22
Shawn Cook <shawn@shawncook.net>
2021-06-20 17:15:43 -0500

    fix notifications, add sunset time

--- checkConditions.py
+++ checkConditions.py
@@ -8,16 +8,25 @@ from datetime import datetime, timezone
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+from suntime import Sun, SunTimeException
+
+"""
+load sample data, if necessary.
+must change for `resp_json` and `one_call_rsp`
+"""
+# hourly_sample = open('hourly.json',)
+# onecall_sample = open('onecall.json',)
+
 api_key   = os.environ.get('API_KEY')
 base_url  = 'http://api.openweathermap.org/data/2.5/forecast?'
 zip_code  = os.environ.get('ZIP_CODE')
 req_url   = base_url + 'appid=' + api_key + '&zip=' + zip_code + '&units=imperial'
-resp_json = requests.get(req_url).json()
+resp_json = requests.get(req_url).json() # json.load(hourly_sample)
 
 # one call, that's all
 loc      = pgeocode.Nominatim('us').query_postal_code(zip_code)
 one_call = 'https://api.openweathermap.org/data/2.5/onecall?lat=%d&lon=%d&exclude=minutely&units=imperial&appid=%s' % (loc.latitude, loc.longitude, api_key)
-one_call_rsp = requests.get(one_call).json()
+one_call_rsp = requests.get(one_call).json() # json.load(onecall_sample)
 
 # email account creds
 email_recipients   = [ os.environ.get('RECIPIENT') ]
@@ -27,47 +36,87 @@ email_smtp_port    = os.environ.get('SMTP_PORT')
 email_smtp_server  = os.environ.get('SMTP_SERVER')
 email_subject      = '🪁 Upcoming kite conditions'
 
-# cloud cover under 85% (overcast).
-def check_cloud_cover(clouds):
-	return clouds < 85
+# get sunrise/set stuff for this location
+sun = Sun(loc.latitude, loc.longitude)
 
-# Must be Friday, Saturday or Sunday.
-def check_day_valid(date):
+"""
+Format temperature
+"""
+def format_temperature(temp):
+	return temp['day'] if type(temp) is dict else temp
+
+"""
+Format date/time to specified format
+"""
+def format_date_time(dt):
+	date_string = '{0:%I:%M%p %A}'.format(dt).lstrip('0')
+	date_string += ' {0:%b %d}'.format(dt).lstrip('0')
+
+	return date_string
+
+"""
+Check for available time outside work.
+"""
+def check_date_time_valid(date):
 	weekday = date.weekday()
-	valid_days = range(4, 7)
-	return weekday in valid_days
+	is_weekday = weekday in range(0,5)
+	if is_weekday:
+		return check_hour_valid(date, 18)
+	else:
+		return check_hour_valid(date, 10)
 
-# Hour must be between 10am and 6pm.
-def check_hours_valid(date):
-	return 10 <= date.hour <= 18
+"""
+Hour must be between specified hour and sunset.
+Default hourStart is 10am.
+"""
+def check_hour_valid(date, hourStart = 10):
+	sunset = sun.get_local_sunset_time(date)
+	return hourStart <= date.hour <= sunset.hour
 
-# check probability of precipitation
+"""
+check probability of precipitation
+"""
 def check_precipitation(pop):
 	return pop < 25
 
-# Temp is between 50 and 90.
+"""
+Check for acceptable temp.
+"""
 def check_temp_valid(temp):
-	parsed = temp['day'] or temp
-	return 50 <= parsed <= 90
+	ideal = {}
+	ideal['min'] = 45
+	ideal['max'] = 95
+	return ideal['min'] <= format_temperature(temp) <= ideal['max']
 
+"""
+Check weather description to find mentions of rain.
+"""
 def check_weather_desc(desc):
-	return not desc.find('rain')
+	return "rain" not in desc
 
-# bonus points for winds from NE to S
+"""
+Earn a star for winds from NE to S.
+"""
 def check_wind_direction(deg):
 	return 45 <= deg <= 180
 
-# wind speed between 8 and 20mph
+"""
+Check for enough wind.
+"""
 def check_wind_speed(wind):
-	return 8 <= wind <= 20
+	return 8 <= wind
 
-# get human readable wind direction
+"""
+Get human readable wind direction.
+"""
 def degrees_to_cardinal(d):
 	dirs = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW']
 	ix = int((d + 11.25)/22.5)
 	return dirs[ix % 16]
 
-# use smtplib to send and email
+"""
+Use smtplib to email notification.
+"""
 def send_email(email_body):
 	server = smtplib.SMTP_SSL(email_smtp_server, email_smtp_port)
 	server.login(email_sender_uname, email_sender_pw)
@@ -86,27 +135,28 @@ def send_email(email_body):
 
 	server.quit()
 
-# validate a weather condition
+"""
+Validate condition
+"""
 def validate_condition(cond):
-	clouds = cond['clouds']
 	dt     = cond['dt']
-	pop    = cond['pop']
+	pop    = cond['pop'] * 100 # percentage
 	wind   = cond['wind_speed']
 	temp   = cond['temp']
 	desc   = cond['weather'][0]['description']
 	date   = datetime.utcfromtimestamp(dt)
 
 	return (
-		check_cloud_cover(clouds)
-		and check_day_valid(date)
-		and check_hours_valid(date)
+		check_date_time_valid(date)
 		and check_precipitation(pop)
 		and check_weather_desc(desc)
 		and check_temp_valid(temp)
 		and check_wind_speed(wind)
 	)
 
-# build list of contitions and data.
+"""
+Build list of contitions and data.
+"""
 def build_datapoint(condition):
 	desc = condition['weather'][0]['description']
 	dt   = datetime.utcfromtimestamp(condition['dt'])
@@ -133,31 +183,24 @@ def build_datapoint(condition):
 	body += (
 		'\r\n'
 		'Conditions: '
-		+ str(round(temp)) + '\u00b0F, ' + str(desc.title())
+		+ str(round(format_temperature(temp))) + '\u00b0F, ' + str(desc.title())
 	)
 
 	return body
 
-# format date/time to specified format
-def format_date_time(dt):
-	date_string = '{0:%I:%M%p %A}'.format(dt).lstrip('0')
-	date_string += ' {0:%b %d}'.format(dt).lstrip('0')
-
-	return date_string
-
 # do the rest
 daily   = one_call_rsp['daily']
 hourly  = one_call_rsp['hourly']
-today   = check_day_valid(datetime.today())
 optimal = []
 
 for condition in hourly:
-	if validate_condition(condition) and today == True:
+	if validate_condition(condition):
 		optimal.append(build_datapoint(condition))
 
 for condition in daily:
 	if validate_condition(condition):
-		optimal.append(build_datapoint(condition))
+		if build_datapoint(condition) not in optimal:
+			optimal.append(build_datapoint(condition))
 
 # if there are optimal dates, do things.
 if len(optimal):
@@ -165,5 +208,4 @@ if len(optimal):
 	message += '\r\n\r\n'.join(optimal)
 	message += '\r\n\r\nHave fun!'
 
-	print(message)
 	send_email(message)
